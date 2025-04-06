5df0bc25075873d5ad2caec47b152ae5cd6629a5
SofiaMArias <sofiaarias0707@gmail.com>
2025-04-06 12:15:01 -0300

    foldr bien y el resto no anda del todo

--- src/Documento.hs
+++ src/Documento.hs
@@ -28,21 +28,50 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
--- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
-foldDoc = error "PENDIENTE: Ejercicio 1"
+-- Ejercicio 1 
+foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
+foldDoc fVacio fTexto fLinea documento = case documento of
+                                              Vacio -> fVacio
+                                              Texto s d -> fTexto s (rec d)
+                                              Linea i d -> fLinea i (rec d)
+                                              where rec = foldDoc fVacio fTexto fLinea
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
+-- ejercicio 2
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = error "PENDIENTE: Ejercicio 2"
+d1 <+> d2 = case d1 of
+              vacio -> procesDoc d2
+              Texto s d -> concatText s (procesDoc d2)
+              Linea i d -> concatLinea i (procesDoc d2)
 
+procesDoc :: Doc -> Doc
+procesDoc = foldDoc Vacio concatText concatLinea;
+
+concatText :: String -> Doc -> Doc
+concatText s d = case d of 
+                  vacio -> Texto s Vacio 
+                  Texto s' doc -> Texto (s ++ s') doc
+                  Linea i doc -> Texto s (Linea i doc)
+
+concatLinea :: Int -> Doc -> Doc
+concatLinea i d = case d of
+                  vacio -> Linea i Vacio
+                  Texto s doc -> Linea i (Texto s doc)
+                  Linea i' doc -> Linea i (Linea i' doc)
+                
+
+-- ejercicio 3
 indentar :: Int -> Doc -> Doc
-indentar i = error "PENDIENTE: Ejercicio 3"
+indentar = indentar
+-- indentar i = foldDoc <+> (Linea i <+>) tiene errores
 
+-- ejercicio 4
 mostrar :: Doc -> String
-mostrar = error "PENDIENTE: Ejercicio 4"
+mostrar = mostrar
+-- mostrar = foldDoc "" (++) (no se)
 
 -- | Función dada que imprime un documento en pantalla
 
