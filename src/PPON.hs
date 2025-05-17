ce7841292992552403af2d93c03b043149852916
Lucas Parma <l.parma93@gmail.com>
2025-05-17 19:19:44 -0300

    ej9: corregir la justificacion del tipo de recursion en pponADoc

--- src/PPON.hs
+++ src/PPON.hs
@@ -45,9 +45,15 @@ aplanar :: Doc -> Doc
 aplanar = foldDoc vacio (\s d -> texto s <+> d) (\_ d -> texto " " <+> d)
 
 -- Justificacion de Recursion
--- Asumiendo que las funciones(aplanar, entreLlaves) utilizadas en la funcion pponADoc no modifican el tipo de recursion
--- podemos decir que la funcion pponADoc utiliza recursion estructural, ya que cumple con tener una funcion
--- para cada constructor, pepon solo se usa en el llamado recursivo y el caso base tiene un valor fijo.
+-- Asumiendo que las funciones (aplanar, entreLlaves) utilizadas en la función pponADoc no modifican el tipo de recursión,
+-- podemos decir que la función pponADoc utiliza recursión primitiva.
+-- Cumple con tener una definición para cada constructor del tipo PPON, 
+-- los casos base tienen un valor fijo que no depende de llamadas recursivas
+-- y en el caso del constructor ObjetoPP xs, 
+-- la funcion accede a la subestructura xs (una lista de pares (s,p)) para mapear su contenido
+-- y procesar explicítamente la clave 's' antes de aplicar la recursion sobre el valor 'p'.
+
+
 pponADoc :: PPON -> Doc
 pponADoc pepon = case pepon of 
                     ObjetoPP xs -> if pponObjetoSimple pepon
@@ -55,5 +61,5 @@ pponADoc pepon = case pepon of
                                     else entreLlaves (recPpon xs)
                     IntPP s -> texto (show s) 
                     TextoPP s -> texto (show s)
-                  where recPpon = map (\(s,p) -> (texto (show s) <+> texto ": " <+> pponADoc p))
+                  where recPpon = map (\(s,p) -> texto (show s) <+> texto ": " <+> pponADoc p)
 
