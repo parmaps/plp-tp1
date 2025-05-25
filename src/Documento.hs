4555327130aed686eee7340e417af021f94a3219
Rocio Han <rophan001@gmail.com>
2025-05-25 10:56:17 -0300

    correccion del invariante ej2

--- src/Documento.hs
+++ src/Documento.hs
@@ -40,16 +40,27 @@ foldDoc fVacio fTexto fLinea documento = case documento of
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
+-- Ejercicio 2
+
 -- Justificacion Invariante
--- Se satisface el Invariante de Doc porque:
--- En el caso de ser texto, el texto aplica la función texto (ya que en la parte que el foldDoc discrimina ese caso se aplica en la función texto en <+>)
--- Si no viene con saltos de línea, no se agregan, dado que texto replica el mismo texto que viene sin agregar nada
--- d ingresa nuevamente al foldDoc con las funciones especificadas en el <+>
--- Siendo línea, entra en el caso de la funcion Linea, que se da de la misma manera en la que ingresa, y de esta manera no cambia el hecho de que i >= 0. Simplemente no se modifica la entrada
--- A su vez, como aclara el PDF de la consigna, nos aseguramos de mantener los invariantes internos usando las funciones que nos evitan exportar los constructores
+-- Se satisface el Invariante de Doc:
+-- En esta funcion trabajamos con foldDoc que esta bien contruido porque trabaja con constructores bien formados 
+-- y no genera nuevos de forma arbitraria.
+
+-- Texto s d: utiliza la funcion concatText que nunca va a generar un Texto s d con s vacío ni con saltos de línea.
+--Esto se garantiza porque en los dos casos posibles:
+--si el segundo argumento (d2) es tambien Texto s' doc, entonces contruye uno nuevo concatenando el s de un documento d1 
+--y el s' d2 (y sabemos que por el constructor s y s' ya cumplen el invariante)
+--sino, se contruye un nuevo Texto con s y un doc que puede ser Vacio o Linea i d y también siguen cumpliendo con el invariante.
+
+--Linea s d: en este caso, trabaja directamente con el contructor Linea y sin modificar del valor de i por lo que podemos 
+--asegurar que i sigue cumpliendo la condición de i >= 0.
+
+--Por último, dado que no se exportan constructores Doc para uso directo, nos aseguramos que en la generación o 
+--concatenación se preserve el invariante.
 
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = foldDoc d2 concatText Linea d1 --
+d1 <+> d2 = foldDoc d2 concatText Linea d1 
 
 
 concatText :: String -> Doc -> Doc
@@ -57,12 +68,20 @@ concatText s d = case d of
                   Texto s' doc -> Texto (s ++ s') doc
                   x -> Texto s x --
                 
+-- Ejercicio 3
 -- Justificacion Invariante
 -- Se mantiene el Invariante de Doc porque:
+
 -- En el caso de ser texto, el string de entrada no se modifica, usando texto para mantener el invariante.
--- con respecto a los saltos de línea, pasa lo mismo. No se modifica el string, y esto significa que no se le agrega ningún salto de línea.
--- D ingresa y por foldDoc entra a indentar de nuevo, no se modifica el invariante con respecto a el, ya que solo se amplían los saltos de línea, el texto y el d no se modifican en esa instancia.
--- En el caso de que sea Línea, se mantiene el invariante de que i>=0, ya que solo se suma un i positivo al número de línea que por invariante de entrada debe ser positivo.
+
+-- con respecto a los saltos de línea, pasa lo mismo. No se modifica el string, y esto significa que no se
+-- le agrega ningún salto de línea.
+
+-- D ingresa y por foldDoc entra a indentar de nuevo, no se modifica el invariante con respecto a el, 
+--ya que solo se amplían los saltos de línea, el texto y el d no se modifican en esa instancia.
+
+-- En el caso de que sea Línea, se mantiene el invariante de que i>=0, ya que solo se suma un
+-- i positivo al número de línea que por invariante de entrada debe ser positivo.
 
 indentar :: Int -> Doc -> Doc
 indentar i = foldDoc Vacio Texto (\i' d -> Linea (i+i') d)
